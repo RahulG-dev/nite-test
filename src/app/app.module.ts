89304b467ca9abae8757a5b9d87bafedcfccdc2c
Rahul <gade.rahult@gmail.com>
2021-07-02 09:48:45 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TypelistComponent } from './typelist/typelist.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BooklistComponent } from './booklist/booklist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypelistComponent,
+    BooklistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/typelist/typelist.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TypelistComponent } from './typelist.component';
+
+describe('TypelistComponent', () => {
+  let component: TypelistComponent;
+  let fixture: ComponentFixture<TypelistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TypelistComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TypelistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
